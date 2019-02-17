aa6adcc0c2a6cd8f9be84bab5d1ee9201383d120
Avaer Kazmer <a@modules.io>
2019-02-16 21:52:49 -0500

    Remove dead dlclose implementation

--- src/main.cpp
+++ src/main.cpp
@@ -36,43 +36,6 @@ napi_value getEventLoopAddress(napi_env env, napi_callback_info args) {
   return result;
 }
 
-napi_value doDlclose(napi_env env, napi_callback_info args) {
-#ifndef LUMIN
-  napi_value argv[1];
-  size_t argc = sizeof(argv)/sizeof(argv[0]);
-  napi_value thisArg;
-  void *data;
-  napi_get_cb_info(env, args, &argc, argv, &thisArg, &data);
-
-  char soPath[32768];
-  size_t length;
-  napi_get_value_string_utf8(env, argv[0], soPath, sizeof(soPath), &length);
-
-#ifndef _WIN32
-  void *handle = dlopen(soPath, RTLD_LAZY);
-  if (handle) {
-    while (dlclose(handle) == 0) {}
-  }
-#else
-  WCHAR soPath_w[32768];
-  MultiByteToWideChar(CP_UTF8, 0, soPath, -1, soPath_w, sizeof(soPath_w)/sizeof(soPath_w[0]));
-
-  HMODULE handle = LoadLibraryExW(soPath_w, NULL, LOAD_WITH_ALTERED_SEARCH_PATH);
-  if (handle) {
-    while (FreeLibrary(handle)) {}
-  }
-#endif
-#else
-  void *handle = nullptr;
-#endif
-
-  mut.unlock();
-
-  napi_value result;
-  napi_get_boolean(env, (bool)handle, &result);
-  return result;
-}
-
 napi_value Init(napi_env env, napi_value exports) {
   uv_loop_t *loop;
   napi_status status = napi_get_uv_event_loop(env, &loop);
@@ -81,12 +44,6 @@ napi_value Init(napi_env env, napi_value exports) {
   napi_value getEventLoopAddressFn;
   napi_create_function(env, NULL, 0, getEventLoopAddress, NULL, &getEventLoopAddressFn);
   napi_set_named_property(env, exports, "getEventLoopAddress", getEventLoopAddressFn);
-  
-  napi_value dlcloseFn;
-  napi_create_function(env, "dlclose", NAPI_AUTO_LENGTH, doDlclose, NULL, &dlcloseFn);
-  napi_set_named_property(env, exports, "dlclose", dlcloseFn);
-  
-  // std::cout << "do init " << loop << std::endl;
   
   return exports;
 }
