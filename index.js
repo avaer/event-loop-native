90402d147af02d5a99d3bc16796179bbcc5a61d8
Avaer Kazmer <a@modules.io>
2019-02-24 01:36:13 -0500

    Add initFunctionAddress exposure

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const path = require('path');
 
 const dlibPath = path.join(__dirname, 'build', 'Release', 'event_loop_native_napi.node');
-const {getEventLoopAddress} = require(dlibPath);
+const {getEventLoopAddress, initFunctionAddress} = typeof requireNative === 'undefined' ? require(dlibPath) : requireNative('event_loop_native_napi.node');
 const eventLoopAddress = getEventLoopAddress();
 eventLoopAddress.getDlibPath = () => dlibPath;
+eventLoopAddress.initFunctionAddress = initFunctionAddress;
 
 module.exports = eventLoopAddress;
--- src/main.cpp
+++ src/main.cpp
@@ -38,6 +38,20 @@ napi_value Init(napi_env env, napi_value exports) {
   napi_value getEventLoopAddressFn;
   napi_create_function(env, NULL, 0, getEventLoopAddress, NULL, &getEventLoopAddressFn);
   napi_set_named_property(env, exports, "getEventLoopAddress", getEventLoopAddressFn);
+
+  uintptr_t address = (uintptr_t)&Init;
+  uint32_t a = (uint32_t)((address >> 32) & 0xFFFFFFFF);
+  uint32_t b = (uint32_t)(address & 0xFFFFFFFF);
+
+  napi_value initFunctionAddress;
+  napi_create_array_with_length(env, 2, &initFunctionAddress);
+  napi_value aValue;
+  napi_create_uint32(env, a, &aValue);
+  napi_set_element(env, initFunctionAddress, 0, aValue);
+  napi_value bValue;
+  napi_create_uint32(env, b, &bValue);
+  napi_set_element(env, initFunctionAddress, 1, bValue);
+  napi_set_named_property(env, exports, "initFunctionAddress", initFunctionAddress);
   
   return exports;
 }
